296a071e458315b5def898a1c130338896ad1732
franselles <fran.selles@gmail.com>
2019-05-20 20:56:09 +0200

    A20190520-A

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,28 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationService } from '../shared/authentication.services';
 import { CoreComponent } from './core.component';
-import { LoggedInGuard  } from './../shared/logged-in.guard';
+import { LoggedInGuard } from './../shared/logged-in.guard';
 
 const routes: Routes = [
-  {path: 'dash', loadChildren: './../core/dash/dash.module#DashModule', canActivate: [LoggedInGuard]},
-  {path: 'login', loadChildren: './../login/login.module#LoginModule'},
-  {path: '**', redirectTo: 'login', pathMatch: 'full'}
+    {
+        path: 'dash',
+        loadChildren: './../core/dash/dash.module#DashModule',
+        canActivate: [LoggedInGuard]
+    },
+    { path: 'login', loadChildren: './../login/login.module#LoginModule' },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes, { enableTracing: false })
-  ],
-  exports: [
-    CoreComponent
-  ],
-  declarations: [
-    CoreComponent
-  ],
-  providers: [
-    LoggedInGuard,
-    AuthenticationService
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes, { enableTracing: false, useHash: true })
+    ],
+    exports: [CoreComponent],
+    declarations: [CoreComponent],
+    providers: [LoggedInGuard, AuthenticationService]
 })
-export class CoreModule { }
+export class CoreModule {}
