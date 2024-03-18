57d0f3736059e1b07cc7909a90c63987eaa11e28
franselles <fran.selles@gmail.com>
2024-03-18 17:14:37 +0100

    Update to 17

--- src/app/shared/logged-in.guard.ts
+++ src/app/shared/logged-in.guard.ts
@@ -1,10 +1,10 @@
 // logged-in.guard.ts
 import {Injectable} from '@angular/core';
-import {Router, CanActivate} from '@angular/router';
+import { Router } from '@angular/router';
 import {AuthenticationService} from './authentication.services';
 
 @Injectable()
-export class LoggedInGuard implements CanActivate {
+export class LoggedInGuard  {
   constructor(private user: AuthenticationService) {
   }
 
